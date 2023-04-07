5ad02b4e2a2fa7ac9e00986343ca60d1f87f4b97
Alencar Xavier <alenxav@gmail.com>
2023-04-07 22:50:02 +0000

    version 2.2

--- src/eigen20220714.cpp
+++ src/eigen20220714.cpp
@@ -3,24 +3,23 @@
 #include <RcppEigen.h>
 
 // [[Rcpp::export]]
-Eigen::MatrixXd EigenARC(Eigen::MatrixXd X, bool centralizeZ = true, int cores = 2){
+Eigen::MatrixXd EigenARC(Eigen::MatrixXd X, bool centralizeX = true, int cores = 2){
+  // cseweb.ucsd.edu/~saul/papers/nips09_kernel.pdf
   Eigen::setNbThreads(cores); int p = X.cols(), n = X.rows(); 
-  double tmp, Npi=3.1416;
-  if(centralizeZ){
+  double tmp, Npi=3.1416, theta, J1, Kij, Norm;
+  if(centralizeX){
     for(int i=0; i<p; i++){
       tmp = (X.col(i).array()).mean();
       X.col(i) = X.col(i).array()-tmp;}}
   Eigen::MatrixXd XXp = X*X.transpose();
   tmp = 1/(XXp.diagonal().mean()); XXp *= tmp;
   Eigen::VectorXd DiagXXp = XXp.diagonal().array();
-  Eigen::VectorXd SqDiagXXp = DiagXXp.array().sqrt();
-  for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-      if(i>j){
-        tmp = acos( XXp(i,j)/(SqDiagXXp(i)*SqDiagXXp(j)) );
-        tmp = (sin(tmp)*DiagXXp[i]*DiagXXp[j])/Npi+(Npi-tmp)*cos(tmp);
-        tmp /= Npi; XXp(i,j) = tmp*1.0; XXp(j,i) = tmp*1.0;
-      }else if(i==j){ XXp(i,j) = 1;}}}
+  for(int i=0; i<n; i++){ for(int j=i; j<n; j++){ 
+    Norm = sqrt(DiagXXp(i)*DiagXXp(j)*1.001);
+    theta = acos( XXp(i,j)/Norm);
+    J1 = sin(theta) + (Npi-theta)*cos(theta);
+    Kij = Norm/Npi*J1;
+    XXp(i,j) = Kij*1.0; XXp(j,i) = Kij*1.0;}}
   return XXp;}
 
 // [[Rcpp::export]]
@@ -65,6 +64,7 @@ SEXP EigenBDCSVD(Eigen::MatrixXd X, int cores = 2){
   Eigen::setNbThreads(cores);
   Eigen::BDCSVD<Eigen::MatrixXd> svd(X, Eigen::ComputeThinU | Eigen::ComputeThinV );
   return Rcpp::List::create(Rcpp::Named("U")=svd.matrixU(),
+                            Rcpp::Named("D")=svd.singularValues(),
                             Rcpp::Named("V")=svd.matrixV());}
 
 // [[Rcpp::export]]
@@ -72,6 +72,7 @@ SEXP EigenJacobiSVD(Eigen::MatrixXd X, int cores = 2){
   Eigen::setNbThreads(cores);
   Eigen::JacobiSVD<Eigen::MatrixXd> svd(X, Eigen::ComputeThinU | Eigen::ComputeThinV );
   return Rcpp::List::create(Rcpp::Named("U")=svd.matrixU(),
+                            Rcpp::Named("D")=svd.singularValues(),
                             Rcpp::Named("V")=svd.matrixV());}
 
 // [[Rcpp::export]]
